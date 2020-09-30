ed97cb44cc2f200d3af4141df5acf103143d62e9
edunnwe1 <edunnwe1@jhmi.edu>
2020-09-29 22:35:36 -0400

    minor tweaks to CNN_model (added depth)

--- scripts/CNN_model.py
+++ scripts/CNN_model.py
@@ -12,6 +12,8 @@ class Resize_Alexnet(nn.Module):
     def __init__(self, depth=4): # (self,input_size = (80,80), pretrained=True)
         super(Resize_Alexnet,self).__init__()
         alexnet = models.alexnet(pretrained=True)
+        if depth==5:
+            self.features = alexnet.features
         if depth==4:
             self.features = alexnet.features[0:9]
         elif depth==3:
@@ -20,11 +22,9 @@ class Resize_Alexnet(nn.Module):
             self.features = alexnet.features[0:4]
         elif depth==1:
             self.features = alexnet.features[0]
-#         self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         
     def forward(self,x):
         x = self.features(x)
-#         x = self.avgpool(x)
         return x
         
 
